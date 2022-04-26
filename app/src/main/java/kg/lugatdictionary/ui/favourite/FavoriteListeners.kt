3a6a7df722f8fa18d17786b6360076e9d96b0350
Emir <tashievemir01.gmail.com>
2022-04-26 12:51:32 +0600

    add recycle logic to favorite fragments

---
+++ app/src/main/java/kg/lugatdictionary/ui/favourite/FavoriteListeners.kt
@@ -0,0 +1,5 @@
+package kg.lugatdictionary.ui.favourite
+
+interface FavoriteListeners {
+    fun onWordClicked(position: Int, title: String)
+}
---
+++ app/src/main/java/kg/lugatdictionary/ui/favourite/FavoriteViewHolder.kt
@@ -0,0 +1,19 @@
+package kg.lugatdictionary.ui.favourite
+
+import kg.lugatdictionary.databinding.ItemFavoriteBinding
+import kg.lugatdictionary.domain.Word
+import kg.lugatdictionary.ui.utils.base.BaseVH
+
+class FavoriteViewHolder(binding: ItemFavoriteBinding, private val listeners: FavoriteListeners,):BaseVH<Word,ItemFavoriteBinding>(binding) {
+    override fun bind(item: Word) {
+        with(binding){
+            tvTransalteOfWord.text=item.word
+            tvExplanationOfTranslate.text=item.explanation
+
+            itemView.setOnClickListener(){
+                listeners.onWordClicked(absoluteAdapterPosition,item.word)
+            }
+        }
+    }
+
+}
